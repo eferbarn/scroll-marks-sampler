fd1c3a74ba8efc67a155ec975c6fd349fb1ee4c7
eferbarn <eferbarndefi@gmail.com>
2024-05-19 17:40:41 +0330

    #3

--- query_wallet_points.py
+++ query_wallet_points.py
@@ -12,7 +12,7 @@ print(f"::set-output name=timestamp::{timestamp}")
 load_dotenv()
 API_KEY = os.getenv('DUNE_API_KEY')
 cooldown = 0.01 # in seconds
-limit = 1000
+limit = 5
 
 url = f'https://api.dune.com/api/v1/query/3745025/results/csv?limit={limit}'
 headers = {"X-Dune-API-Key": API_KEY}
@@ -20,6 +20,19 @@ response = requests.get(url, headers=headers).text
 
 with open('./addresses.csv', 'w') as file:
     file.write(response)
+
+
+def sort_csv(input_file, output_file, sort_key):
+    with open(input_file, 'r', newline='') as infile:
+        reader = csv.DictReader(infile)
+        data = list(reader)
+
+    data.sort(key=lambda x: x[sort_key])
+
+    with open(output_file, 'w', newline='') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
 
 
 def csv_to_json(csv_file_path, json_file_path):
@@ -32,10 +45,12 @@ def csv_to_json(csv_file_path, json_file_path):
     with open(json_file_path, 'w', encoding='utf-8') as jsonfile:
         json.dump(data, jsonfile, ensure_ascii=False)
 
+
 def query_wallet_points(address):
     url = f"https://kx58j6x5me.execute-api.us-east-1.amazonaws.com/scroll/wallet-points?walletAddress={address}"
     response = requests.get(url)
     return response.json()
+
 
 def main():
     result_file_path = f'./results/results-{timestamp}.csv'
@@ -69,8 +84,10 @@ def main():
 
         index += 1
     
+    sort_csv(result_file_path, result_file_path, 'points')
     csv_to_json(result_file_path, './results.json')
     print(f"Copied {result_file_path} to ./results.json")
+
 
 if __name__ == "__main__":
     try:
